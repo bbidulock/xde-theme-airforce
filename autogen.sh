6d29b12e1642b9a6cfae5a64fb9adc7ba1cdefb6
Brian Bidulock <bidulock@openss7.org>
2013-12-23 13:08:36 -0700

    release handling

---
+++ autogen.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+VERSION=
+
+if [ -z "$VERSION" ]; then
+	VERSION='1.1'
+	if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
+		VERSION=$(git describe --tags|sed 's,[-_],.,g;s,\.g.*$,,')
+		(
+		   echo -e "# created with git log --stat=76 | fmt -sct -w80\n"
+		   git log --stat=76 | fmt -sct -w80
+		)>ChangeLog
+	fi
+fi
+
+sed -i -e "s:[[]xde-theme-airforce[]],[[][^]]*[]]:[xde-theme-airforce],[$VERSION]:
+	   s:AC_REVISION([[][^]]*[]]):AC_REVISION([$VERSION]):" configure.ac
+
+autoreconf -fiv
